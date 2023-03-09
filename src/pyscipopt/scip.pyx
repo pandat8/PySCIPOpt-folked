9dd554f33ae701fe54e0b8e0d2d078448fd60a62
Defeng <pandat8@gmail.com>
2023-03-08 20:42:40 -0500

    upd

--- src/pyscipopt/scip.pyx
+++ src/pyscipopt/scip.pyx
@@ -1161,7 +1161,7 @@ cdef class Model:
 
     def getReadingTime(self):
         """Retrieve the current reading time in seconds"""
-        return SCIPgetReadingfTime(self._scip)
+        return SCIPgetReadingTime(self._scip)
 
     def getPresolvingTime(self):
         """Retrieve the curernt presolving time in seconds"""
